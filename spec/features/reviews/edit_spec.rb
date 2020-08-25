09e3a598768fc64007837a3e5b4cbe1f65a241b7
Austin Aspaas <araspaas@live.com>
2020-08-25 16:08:14 -0600

    Add test for upadating a review

---
+++ spec/features/reviews/edit_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "When I visit a shelter Show page", type: :feature do
+  it "can update a review" do
+    shelter_1 = Shelter.create(name:       "New Hope Rescue",
+                              address:     "208 Main St",
+                              city:        "Denver",
+                              state:       "CO",
+                              zip:         "80329")
+
+    review_1 = shelter_1.reviews.create(
+                        title:   "Git Gud",
+                        rating:  "2",
+                        content:    "Customer service is awful",
+                      )
+    visit "/shelters/#{shelter_1.id}"
+
+    expect(page).to have_content("Update Review")
+
+    click_link 'Update Review'
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}/#{review_1.id}/edit")
+
+    fill_in 'title',  with:     "Got better"
+
+    click_on "Update Review"
+
+    expect(current_path).to eq("/shelters/#{shelter_1.id}")
+    expect(page).to have_content("Got better")
+    expect(page).to have_content("2")
+    expect(page).to have_content("Customer service is awful")
+  end
+end
