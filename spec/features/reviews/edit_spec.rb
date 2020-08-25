c5311259e43b7f20fe95e5da99c41bc892483cdb
Corey Carter <crc2231@gmail.com>
2020-08-25 17:33:51 -0600

    Add flash message testing to new and edit review

--- spec/features/reviews/edit_spec.rb
+++ spec/features/reviews/edit_spec.rb
@@ -30,4 +30,30 @@ RSpec.describe "When I visit a shelter Show page", type: :feature do
     expect(page).to have_content("2")
     expect(page).to have_content("Customer service is awful")
   end
+
+  describe 'When I visit the update review form by clicking a link on the show' do
+
+    it 'I can not update an review without a title' do
+      shelter_1 = Shelter.create(name:       "New Hope Rescue",
+        address:     "208 Main St",
+        city:        "Denver",
+        state:       "CO",
+        zip:         "80329")
+
+        review_1 = shelter_1.reviews.create(
+                            title:   "Git Gud",
+                            rating:  "2",
+                            content:    "Customer service is awful",
+                          )
+
+      visit "/shelters/#{shelter_1.id}"
+
+      click_on 'Update Review'
+
+      fill_in 'title',  with:     ""
+
+      click_on 'Update Review'
+      expect(page).to have_content("Title, rating, and content required to continue!")
+    end
+  end
 end
--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -3,25 +3,42 @@ require 'rails_helper'
 RSpec.describe "From Shelter Show Page", type: :feature do
   it "can create a new review" do
     shelter_1 = Shelter.create(name:       "New Hope Rescue",
-                              address:     "208 Main St",
-                              city:        "Denver",
-                              state:       "CO",
-                              zip:         "80329")
+      address:     "208 Main St",
+      city:        "Denver",
+      state:       "CO",
+      zip:         "80329")
 
-    visit "/shelters/#{shelter_1.id}"
+      visit "/shelters/#{shelter_1.id}"
 
-    click_link 'Add Review'
-    expect(current_path).to eq("/shelters/#{shelter_1.id}/review")
-    fill_in 'title', with: "This place rocks."
-    fill_in 'rating',      with: 4
-    fill_in 'content',     with: "This place has everything you need"
-    fill_in 'image',       with: "https://www.gannett-cdn.com/presto/2020/03/26/PWES/b54dc23e-34f6-460e-8931-26ccc2f8677f-sh032620coronapets005.JPG?crop=6213,3495,x0,y389&width=3200&height=1801&format=pjpg&auto=webp"
+      click_link 'Add Review'
+      expect(current_path).to eq("/shelters/#{shelter_1.id}/review")
+      fill_in 'title', with: "This place rocks."
+      fill_in 'rating',      with: 4
+      fill_in 'content',     with: "This place has everything you need"
+      fill_in 'image',       with: "https://www.gannett-cdn.com/presto/2020/03/26/PWES/b54dc23e-34f6-460e-8931-26ccc2f8677f-sh032620coronapets005.JPG?crop=6213,3495,x0,y389&width=3200&height=1801&format=pjpg&auto=webp"
 
-    click_on "Add Review"
-    expect(current_path).to eq("/shelters/#{shelter_1.id}")
-    expect(page).to have_content("This place rocks.")
-    expect(page).to have_content(4)
-    expect(page).to have_content("This place has everything you need")
-    expect(page).to have_css("img[src*='https://www.gannett-cdn.com/presto/2020/03/26/PWES/b54dc23e-34f6-460e-8931-26ccc2f8677f-sh032620coronapets005.JPG?crop=6213,3495,x0,y389&width=3200&height=1801&format=pjpg&auto=webp']")
+      click_on "Add Review"
+      expect(current_path).to eq("/shelters/#{shelter_1.id}")
+      expect(page).to have_content("This place rocks.")
+      expect(page).to have_content(4)
+      expect(page).to have_content("This place has everything you need")
+      expect(page).to have_css("img[src*='https://www.gannett-cdn.com/presto/2020/03/26/PWES/b54dc23e-34f6-460e-8931-26ccc2f8677f-sh032620coronapets005.JPG?crop=6213,3495,x0,y389&width=3200&height=1801&format=pjpg&auto=webp']")
+    end
+
+    describe 'When I visit the new review form by clicking a link on the show' do
+
+      it 'I can not create an review without a title' do
+        shelter_1 = Shelter.create(name:       "New Hope Rescue",
+          address:     "208 Main St",
+          city:        "Denver",
+          state:       "CO",
+          zip:         "80329")
+
+        visit "/shelters/#{shelter_1.id}"
+
+        click_on 'Add Review'
+        click_on 'Add Review'
+        expect(page).to have_content("Title, rating, and content required to continue!")
+      end
+    end
   end
-end
