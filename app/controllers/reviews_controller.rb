24c6c71eeadb3edd5cc0334a5c0cebe24f0946b8
Corey Carter <crc2231@gmail.com>
2020-08-24 18:32:00 -0600

    Create review controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,37 @@
+class ReviewsController < ApplicationController
+
+  def new
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def create
+    Review.create(review_params)
+    redirect_to "/shelters/#{@shelter.id}"
+  end
+
+  def review_params
+    params.permit(:title, :rating, :content, :image)
+  end
+  #
+  # def edit
+  #   @review = Review.find(params[:id])
+  # end
+  #
+  # def update
+  #   review = Review.find(params[:id])
+  #   review.update({name: params[:name], address: params[:address], city: params[:city], state: params[:state],zip: params[:zip]})
+  #   review.save
+  #   # redirect_to "/reviews/#{review.id}"
+  # end
+  #
+  # #
+  # def destroy
+  #   Review.destroy(params[:id])
+  #   redirect_to '/shelters'
+  # end
+
+
+end
