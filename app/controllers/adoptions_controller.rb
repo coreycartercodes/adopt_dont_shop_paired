703da1b0d652043eafbbc72df450fd0a158032a9
Corey Carter <crc2231@gmail.com>
2020-08-30 16:51:26 -0600

    Create new and create action in adoptions controller

---
+++ app/controllers/adoptions_controller.rb
@@ -0,0 +1,32 @@
+class AdoptionsController < ApplicationController
+
+  def new
+    @favorite_pets = favorites.contents.map do |favorite|
+      new_fav = Pet.find(favorite)
+    end
+  end
+
+  def create
+    @adoption = Adoption.new(adoptions_params)
+    params[:apply_status].each do |pet_id, value|
+      if value == "1"
+        @pet = Pet.find(pet_id)
+        @adoption.pets << @pet
+        session[:favorites].delete(@pet.id.to_s)
+      end
+    end
+    if @adoption.save
+
+      flash[:save_alert] = "Application Submitted Successfully!"
+      redirect_to '/favorites'
+    else
+      flash[:warning] = "All fields required to continue!"
+      redirect_to '/adoptions/new'
+    end
+  end
+
+  def adoptions_params
+    params.permit(:name, :address, :city, :state, :zip, :phone_number, :description, :pets)
+  end
+
+end
