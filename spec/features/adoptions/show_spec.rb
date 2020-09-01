a07c86fd852dafce76e7d62a1edd14edcd04ead2
Corey Carter <crc2231@gmail.com>
2020-08-31 18:24:39 -0600

    Add test for Pending Application message when already approved

--- spec/features/adoptions/show_spec.rb
+++ spec/features/adoptions/show_spec.rb
@@ -83,4 +83,26 @@ RSpec.describe "As a visitor to an adoptions show page", type: :feature do
   #   expect(page).to have_content("Pending")
   #   expect(page).to have_content("On hold for #{adoption_1.name}")
   # end
+
+  it "will only allow one approved application" do
+    shelter_1 = Shelter.create(name:       "New Hope Rescue",
+                              address:     "208 Main St",
+                              city:        "Denver",
+                              state:       "CO",
+                              zip:         "80329")
+
+    pet_1 = shelter_1.pets.create(
+                        name:   "Fido",
+                        image:  "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg",
+                        age:    "2",
+                        sex:       "Male",
+                        adoption_status: "Pending"
+                      )
+
+    adoption_1 = pet_1.adoptions.create(name: "Mike Dao", address: "345 Mike Dao Rd.", city: "Denver", state: "CO", zip: "99999", phone_number: "123-456-7890", description: "Mike Dao")
+
+    visit "/adoptions/#{adoption_1.id}"
+    expect(page).to have_content("Pending Application for #{pet_1.name}. Call for more details.")
+  end
+
 end
