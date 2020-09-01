8e649318d4dfab0f66c3c2361a6cc45c83912eda
Corey Carter <crc2231@gmail.com>
2020-09-01 17:58:11 -0600

    Add delete pets method to shelter model

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,4 +1,11 @@
 class Shelter < ApplicationRecord
   has_many :pets
   has_many :reviews
+
+  def delete_pets
+    pets.each do |pet|
+      pet.adoptions.clear
+      pet.delete
+    end
+  end
 end
