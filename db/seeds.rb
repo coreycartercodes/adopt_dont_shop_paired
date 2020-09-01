2378d855b99d3b773d130fce19ed3481261225bc
Corey Carter <crc2231@gmail.com>
2020-09-01 17:16:39 -0600

    Add images to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 @shelter1 = Shelter.create(name: "Van's pet shop", address: "3724 tennessee dr", city: "Rockford", state: "Illinois", zip: "61108")
 @shelter2 = Shelter.create(name: "Bovice's pet shop", address: "37 ed vera dr", city: "Rockford", state: "Illinois", zip: "61109")
-@shelter1.pets.create(image: "Bellas-pic", name: "Bella", age: "5", sex: "female", description: "Fun Loving Dog")
-@shelter1.pets.create(image: "maisys-pic", name: "Maisy", age: "6", sex: "female", description: "Stomache on legs")
-@shelter2.pets.create(image: "Mr.cats-pic", name: "Mr. Cat", age: "9", sex: "male", description: "Has Russian accent")
-@shelter2.pets.create(image: "franks-pic", name: "Frank", age: "3", sex: "male", description: "He's a snake. does snake things")
+@shelter1.pets.create(image: "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/article_thumbnails/other/dog_cool_summer_slideshow/1800x1200_dog_cool_summer_other.jpg", name: "Bella", age: "5", sex: "female", description: "Fun Loving Dog")
+@shelter1.pets.create(image: "https://static.insider.com/image/5d24d6b921a861093e71fef3.jpg", name: "Maisy", age: "6", sex: "female", description: "Stomache on legs")
+@shelter2.pets.create(image: "https://media.wired.com/photos/5cdefb92b86e041493d389df/master/pass/Culture-Grumpy-Cat-487386121.jpg", name: "Mr. Cat", age: "9", sex: "male", description: "Has Russian accent")
+@shelter2.pets.create(image: "https://i.dailymail.co.uk/1s/2020/03/20/13/26207684-0-image-a-4_1584711978894.jpg", name: "Frank", age: "3", sex: "male", description: "He's a snake. does snake things")
 @shelter1.reviews.create(title: "Best Place In Town",
                           rating: 3,
                           content: "We went here to find our new best friend and we were successful. Yay.",
