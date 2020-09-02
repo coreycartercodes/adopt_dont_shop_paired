169fdca14eeee3b85bff0bfcba253faf825a1bfb
Austin Aspaas <araspaas@live.com>
2020-09-01 20:21:29 -0600

    Add validates presence of attributes

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,4 +1,10 @@
 class Shelter < ApplicationRecord
+  validates_presence_of :name
+  validates_presence_of :address
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip
+
   has_many :pets
   has_many :reviews
 
