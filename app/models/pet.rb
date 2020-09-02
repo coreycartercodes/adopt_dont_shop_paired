35bb04c621fa4ab2d287b6385f66253895006bff
Corey Carter <crc2231@gmail.com>
2020-09-01 20:33:22 -0600

    Add validation for attributes

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,9 @@ class Pet < ApplicationRecord
   belongs_to :shelter
   has_many :adoption_pets
   has_many :adoptions, through: :adoption_pets
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :age
+  validates_presence_of :sex
+  validates_presence_of :adoption_status
 end
